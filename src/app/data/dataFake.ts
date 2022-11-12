3c91a7de6c3e586772f9bc6bfd0eb8e7dee42243
harryazawa <victorlbamorim@gmail.com>
2022-11-12 20:05:50 -0300

    refactor: adding big and small text examples

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
-    "id":1,
-    "photo":"aaa",
-    "title":"saiu novo filme",
-    "description":"hello world"
+    "id":"1",
+    "photoCover":"./assets/images/1.jpg",
+    "title":"Placeholder de Título da Notícia 1",
+    "description":"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Phasellus faucibus scelerisque eleifend donec pretium vulputate. Feugiat in ante metus dictum at tempor commodo ullamcorper. Quisque non tellus orci ac auctor augue mauris augue. Urna duis convallis convallis tellus. Fermentum et sollicitudin ac orci phasellus egestas tellus rutrum tellus. Bibendum at varius vel pharetra vel turpis nunc eget. Eget aliquet nibh praesent tristique magna sit. Morbi quis commodo odio aenean. Sagittis eu volutpat odio facilisis. Eu volutpat odio facilisis mauris sit. Augue ut lectus arcu bibendum at. Dolor sit amet consectetur adipiscing. Libero volutpat sed cras ornare arcu. Risus commodo viverra maecenas accumsan. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Mattis nunc sed blandit libero volutpat sed cras ornare. Blandit turpis cursus in hac habitasse. Eget mi proin sed libero enim sed faucibus turpis in. Diam donec adipiscing tristique risus nec.<br/></p><p>Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Feugiat in ante metus dictum at tempor. Sem et tortor consequat id porta nibh. Mattis aliquam faucibus purus in. Risus commodo viverra maecenas accumsan lacus. Amet risus nullam eget felis eget nunc lobortis mattis aliquam. Erat velit scelerisque in dictum non consectetur a erat nam. Nunc non blandit massa enim nec dui. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Placerat vestibulum lectus mauris ultrices eros in cursus.<br/></p><p>Vulputate eu scelerisque felis imperdiet proin fermentum. Cursus turpis massa tincidunt dui ut ornare. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Ultricies mi eget mauris pharetra et ultrices. Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Lacinia at quis risus sed vulputate odio ut enim blandit. Faucibus a pellentesque sit amet. A lacus vestibulum sed arcu non odio euismod lacinia. Massa placerat duis ultricies lacus sed turpis tincidunt id aliquet. Tempus imperdiet nulla malesuada pellentesque elit.</p>"
   },
   {
-    "id":2,
-    "photo":"aaa",
-    "title":"saiu novo filme",
+    "id":"2",
+    "photoCover":"https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png",
+    "title":"placeholder do small card",
     "description":"hello world"
   },
 ]
